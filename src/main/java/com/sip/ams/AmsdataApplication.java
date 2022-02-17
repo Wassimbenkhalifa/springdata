7b505af14b27facc53ecbe89b193c6a4d0979284
unknown <PC@admin52.cmd.emaa>
2022-02-17 23:37:35 +0100

    deployement-tomcat

--- src/main/java/com/sip/ams/AmsdataApplication.java
+++ src/main/java/com/sip/ams/AmsdataApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmsdataApplication {
+public class AmsdataApplication extends SpringBootServletINitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmsdataApplication.class, args);
